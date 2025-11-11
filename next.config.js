dfd1816fca9af804de48b7994268a333483f9c84
Ian Njenga <bnjeynga2@gmail.com>
2025-11-11 12:50:11 -0500

    Add redirect from Vercel URL to njengaventures.dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(?!njengaventures\\.dev).*\\.vercel\\.app',
+          },
+        ],
+        destination: 'https://njengaventures.dev/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
